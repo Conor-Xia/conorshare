6c538d2f1037c6f231d691c8ea41724b98b5c1dc
Conor <38096133+Conor-Xia@users.noreply.github.com>
2021-11-21 13:20:43 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '8436e615b4648ecb843c6538c72aa5b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'conor-share',
+  domain: 'conor-share.it',
+  author: 'Conor',
 
   // open graph metadata (optional)
   description: 'Example site description',
